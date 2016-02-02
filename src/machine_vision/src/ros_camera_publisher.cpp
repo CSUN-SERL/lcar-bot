6b880ed2cd9f848912e98199607371df80c9f9d1
mtbrobotanist <floydwin7@gmail.com>
2016-02-01 16:58:58 -0800

    Added stereo camera support using libuvc. Need to calibrate the camera and rectify its images to get depth information.

--- src/machine_vision/src/ros_camera_publisher.cpp
+++ src/machine_vision/src/ros_camera_publisher.cpp
@@ -1,17 +1,20 @@
 #include <ros/ros.h>
+#include <ros/console.h>
+#include <cv_bridge/cv_bridge.h>
+
 #include <image_transport/image_transport.h>
 #include "opencv2/highgui.hpp"
-#include <cv_bridge/cv_bridge.h>
 #include <sstream> // for converting the command line parameter to integer#
 #include <stdlib.h>
 
 #include <iostream>
 
+using namespace std;
 
 int main(int argc, char** argv)
 {
 
-  ros::init(argc, argv, "stereo_camera", ros::init_options::AnonymousName);
+  ros::init(argc, argv, "stereo_cam", ros::init_options::AnonymousName);
   ros::NodeHandle nh;
   image_transport::ImageTransport it(nh);
 
@@ -22,15 +25,16 @@ int main(int argc, char** argv)
 
 
   if(!paramSet){
-      topic = "stereo_cam_default";
+      topic = "mono_cam/image_raw";
   }
   image_transport::Publisher pub = it.advertise(topic, 1);
 
   int video_source;
   paramSet = ros::param::get("~video_id", video_source);
   if(!paramSet){
-      video_source = -1;
+      video_source = 0;
   }
+  cout << video_source << "\n";
   cv::VideoCapture cap(video_source);
   // Check if video device can be opened with the given index
   if(!cap.isOpened()){
@@ -40,16 +44,24 @@ int main(int argc, char** argv)
   cv::Mat frame;
   sensor_msgs::ImagePtr msg;
 
-  //ros::Rate loop_rate(1);
+  int numFrames = 0;
   while (nh.ok()) {
-    cap >> frame;
+    try{
+        cout << "attempting frame capture\n";
+        cap >> frame;
     // Check if grabbed frame is actually full with some content
-    if(!frame.empty()) {
-      msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
-      pub.publish(msg);
-      cv::waitKey(1);
-    }
+        if(!frame.empty()) {
+            msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
+            pub.publish(msg);
 
+            cout << numFrames++ << endl;
+        }
+        else cout << "frame skipped\n";
+    }
+    catch(cv::Exception e){
+        e.formatMessage();
+        ROS_ERROR("%s", e.what());
+    }
     ros::spinOnce();
     //loop_rate.sleep();
   }
--- src/machine_vision/src/ros_camera_subscriber.cpp
+++ src/machine_vision/src/ros_camera_subscriber.cpp
@@ -7,23 +7,23 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
   try
   {
-    cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
-    cv::waitKey(5);
+    cv::imshow("view", cv_bridge::toCvShare(msg)->image);
+    cv::waitKey(1);
   }
   catch (cv_bridge::Exception& e)
   {
-    ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+    ROS_ERROR("Error in image subsrcriber: %s", e.what());
   }
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "image_listener");
+  ros::init(argc, argv, "image_listener", ros::init_options::AnonymousName);
   ros::NodeHandle nh;
   cv::namedWindow("view");
   cv::startWindowThread();
   image_transport::ImageTransport it(nh);
-  image_transport::Subscriber sub = it.subscribe("camera/image", 1, imageCallback);
+  image_transport::Subscriber sub = it.subscribe("mono_cam/image_raw", 1, imageCallback);
   ros::spin();
   cv::destroyWindow("view");
 }
