3a1ff6bba0f45d0566f3aeab7f1f47dd18dbe9b5
Edwin Portillo <edwinportillo62@yahoo.com>
2016-02-08 15:03:14 -0800

    GPS now dynamically adds radio buttons and is working correctly

--- src/rqt_gcs/src/rqt_gcs/gps_publisher.cpp
+++ src/rqt_gcs/src/rqt_gcs/gps_publisher.cpp
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
   ros::NodeHandle nh;
   int num_uav = 10;
   int cur_uav = 0;
-  float lat =30, lon = -118;
+  float lat =30, lon = -115;
   ros::Rate loop_rate(10*num_uav); //10Hz per topic
   std::string uav_ns  = "UAV";
   ros::Publisher pub_pos_global[num_uav];
@@ -25,14 +25,14 @@ int main(int argc, char **argv)
 
     pub_pos_global[i] = nh.advertise<sensor_msgs::NavSatFix>(uav_ns + std::to_string(i+1) + "/mavros/global_position/global",10);
 
-    lat += .0005;
-    lon += .0005;
+    lat += .005;
+    lon += .005;
   }
 
   while(ros::ok())
   {
-    position[cur_uav].longitude += 0.00001;
-    position[cur_uav].latitude += 0.00001;
+    position[cur_uav].longitude += 0.000001;
+    position[cur_uav].latitude += 0.000001;
 
     pub_pos_global[cur_uav].publish(position[cur_uav]);
 
