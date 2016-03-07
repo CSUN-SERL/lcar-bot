8366a0e7cbf08c2da57d4ed75243bcb9a7c0aff5
0284374 <thomas.jentis@gmail.com>
2016-03-06 21:47:06 -0800

    updated object detection node going to test tomorrow morning

--- src/machine_vision/src/object_detect.cpp
+++ src/machine_vision/src/object_detect.cpp
@@ -27,11 +27,47 @@ vector< float > hog_detector;
 Scalar reference(0, 255, 0);
 HOGDescriptor hog; //(Size( 90, 160 ), Size(16, 16), Size(8, 8), Size(8, 8), 9)
 //vector <cv::Mat > images;
-//Mat src;
+Mat src;
 //bool start_detection = false;
+vector<Mat> trainingdata;
+const Size & img_size = Size(128, 256);
 
 int frame_id;
 double avg;
+
+
+/////////////////////////////////////////////////////////////////////////////
+void HogFeatureExtraction(Mat ImgMat) {
+    HOGDescriptor d;
+    d.winSize = img_size;
+    vector< Point > location;
+    vector< float > descriptors;
+    d.compute(ImgMat, descriptors, Size(8, 8), Size(0, 0), location);
+    Mat hog_features = Mat(descriptors).clone();
+    if (!hog_features.empty()){
+        trainingdata.push_back(hog_features.reshape(1,1));
+    }
+}
+
+void ConvertToMl(const std::vector< cv::Mat > & train_samples, cv::Mat& trainData) {
+    //--Convert data
+    const int rows = (int) train_samples.size();
+    const int cols = (int) std::max(train_samples[0].cols, train_samples[0].rows);
+    cv::Mat tmp(1, cols, CV_32FC1); //< used for transposition if needed
+    trainData = cv::Mat(rows, cols, CV_32FC1);
+    vector< Mat >::const_iterator itr = train_samples.begin();
+    vector< Mat >::const_iterator end = train_samples.end();
+    for (int i = 0; itr != end; ++itr, ++i) {
+        CV_Assert(itr->cols == 1 ||
+                itr->rows == 1);
+        if (itr->cols == 1) {
+            transpose(*(itr), tmp);
+            tmp.copyTo(trainData.row(i));
+        } else if (itr->rows == 1) {
+            itr->copyTo(trainData.row(i));
+        }
+    }
+}
 
 //Draw detected objects on a window
 void DrawLocations(cv::Mat & img, const std::vector< Rect > & found, const vector<double> weights, const Scalar & color) {
@@ -46,43 +82,73 @@ void DrawLocations(cv::Mat & img, const std::vector< Rect > & found, const vecto
           }
     }
 }
-/////////////////////////////////////////////////////////////////////////////
 
+void ObjectCategorize(cv::Mat image){
+  vector <double> weights;
+  vector <Rect> detected_objects;
+  vector< Rect > door_objects;
+  hog.detectMultiScale(
+          image,
+          detected_objects,
+          weights
+          ,0         // hit threshold
+          ,Size(0,0) // step size
+          ,Size(0,0) // padding
+          ,1.05       // scale factor
+          ,2       // final threshold
+          ,false      // use mean shift grouping?
+  );
+  for(int i = 0; i < detected_objects.size(); i++){
+     Rect windows(detected_objects.at(i).x, detected_objects.at(i).y, detected_objects.at(i).width, detected_objects.at(i).height);
+     Mat Roi = image(windows);
+     Mat extract, res, train_data;
+     resize(Roi, extract, img_size);
+     HogFeatureExtraction(extract);
+     ConvertToMl(trainingdata, train_data);
+     trainingdata.clear();
+     svm->predict(train_data, res, 4);
+     for (int j = 0; j < res.rows; j++) {
+            if (res.at<float>(j, 0) == 0) {
+                door_objects.push_back(detected_objects.at(i));
+            }
+    }
+    res.release();
+    train_data.release();
+    extract.release();
+  }
+  DrawLocations(src, door_objects, weights, Scalar(0,255,0));
+
+}
+
+void ImageShow(){
+  imshow("view", src);
+  cv::waitKey(1);
+}
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
   try
   {
     cv::Mat image;
-      
     if(frame_id++ % 1 == 0){
         image = cv_bridge::toCvCopy(msg, "mono8")->image;
-        vector <double> weights;
-        vector <Rect> detected_objects;
+        src = image.clone();
+        thread thread_1(ObjectCategorize, image);
+        thread thread_2(ImageShow);
 //        vector <Point> detected_objects;
         double t = (double)cvGetTickCount();
 //        hog.detectMultiScale(image, detected_objects, false);
 
-        hog.detectMultiScale(
-                image, 
-                detected_objects,
-                weights
-                ,0         // hit threshold
-                ,Size(0,0) // step size
-                ,Size(0,0) // padding
-                ,1.05       // scale factor
-                ,2       // final threshold
-                ,false      // use mean shift grouping?
-        );
-        
+
+
 //        hog.detect(
-//                image, 
+//                image,
 //                detected_objects,
 //                weights
-//                //,0 
-//                //,Size(8,8) 
-//                //,Size(0,0) 
-//                //,1.5 
-//                //,1.0 
+//                //,0
+//                //,Size(8,8)
+//                //,Size(0,0)
+//                //,1.5
+//                //,1.0
 //                //,true
 //        );
         t = cvGetTickCount() - t;
@@ -92,13 +158,9 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
             cout << "detection time = " << avg / 10 << endl;
             avg = 0;
         }
-        DrawLocations(image, detected_objects, weights, Scalar(0,255,0));
     }
     else
         image = cv_bridge::toCvShare(msg, "mono8")->image;
-
-    imshow("view", image);
-    cv::waitKey(1);
   }
   catch (cv_bridge::Exception& e)
   {
@@ -135,9 +197,9 @@ int main (int argc, char** argv){
   std::string topic;
   if(argc >= 2)
       topic = argv[1];
-  else 
+  else
       topic = "stereo_cam/left/image_raw";
-  
+
   string svmPath = ros::package::getPath("machine_vision");
   svm = StatModel::load<SVM>( svmPath + "/LinearHOG.xml" );
 
