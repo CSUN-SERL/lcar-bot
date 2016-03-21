6635a24b338edf638f963fd7235881fd0f70f045
Edwin Portillo <edwinportillo62@yahoo.com>
2016-03-20 18:50:38 -0700

    updating

--- src/rqt_gcs/src/rqt_gcs/gps_publisher.cpp
+++ src/rqt_gcs/src/rqt_gcs/gps_publisher.cpp
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "gps_publisher");
   ros::NodeHandle nh;
-  int num_uav =99;
+  int num_uav = 99;
   int cur_uav = 0;
   int boolean = 0;
   float lat =0, lon = 0;
@@ -27,26 +27,27 @@ int main(int argc, char **argv)
     pub_pos_global[i] = nh.advertise<sensor_msgs::NavSatFix>(uav_ns + std::to_string(i) + "/mavros/global_position/global",10);
 
       if( (i) % 10 == 0){
-        lat = rand() % 170 + (-85); //-90 to 90
-        lon = rand() % 355 + (-175); //-180 to 180
-      } else {
-        lat += .0001;
-        lon += .0001;
+        if(i == 0){
+          lat = 34.24203265889959;
+          lon = -118.52938055992126;
+        }else{
+          lat = rand() % 170 + (-85); //-90 to 90
+          lon = rand() % 355 + (-175); //-180 to 180
+        }
+    }else {
+        lat += .001;
+        lon += .001;
       }
   }
 
   while(ros::ok())
   {
 
-    if(boolean == 1){
-    position[cur_uav].longitude += 0.00001;
-    position[cur_uav].latitude += 0.00001;
-    boolean = boolean - 1;
-  }else{
-    position[cur_uav].longitude -= 0.00001;
-    position[cur_uav].latitude -= 0.00001;
-    boolean = boolean + 1;
-  }
+    //if(boolean == 1){
+    position[cur_uav].longitude += 0.000001;
+    position[cur_uav].latitude += 0.000001;
+    //boolean = boolean - 1;
+  //}
 
     pub_pos_global[cur_uav].publish(position[cur_uav]);
 
