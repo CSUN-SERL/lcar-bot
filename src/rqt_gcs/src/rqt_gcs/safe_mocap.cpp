33502208b63e30279fe49f88c97420e2fd085150
Karanvir Panesar <singh.karanvir21@gmail.com>
2016-03-25 00:56:24 -0700

    Removed spike detection and added publisher for violations.

--- src/rqt_gcs/src/rqt_gcs/safe_mocap.cpp
+++ src/rqt_gcs/src/rqt_gcs/safe_mocap.cpp
@@ -12,13 +12,9 @@
 #define Y_LIMIT 0.5         // 0.5 meter from the origin
 #define Z_LIMIT 1           // 1 meter from the origin
 #define SAFETY_RADIUS 0.5   // Sphere with 0.5m radius
-#define SPIKE_LIMIT 0.05    // Limit for spikes in data
 
-ros::Publisher pub_mocap;
+ros::Publisher pub_mocap, pub_violation;
 ros::Subscriber sub_vrpn;
-
-geometry_msgs::Pose pose_prev;
-bool pos_received = false;
 
 void VrpnCallback(const geometry_msgs::PoseStamped& msg_pose);
 
@@ -38,6 +34,7 @@ int main(int argc, char **argv)
 
   std::string ns = DEF_NS + std::to_string(id); //UAV Namespace
   pub_mocap = nh.advertise<geometry_msgs::PoseStamped>(ns + "/mavros/mocap/pose",QUEUE_SIZE);
+  pub_violation = nh.advertise<geometry_msgs::Vector3>(ns + "/mocap/violation",QUEUE_SIZE);
   sub_vrpn  = nh.subscribe("vrpn_client_node/" + ns + "/pose", QUEUE_SIZE, VrpnCallback);
 
   while(ros::ok())
@@ -50,34 +47,13 @@ int main(int argc, char **argv)
 
 void VrpnCallback(const geometry_msgs::PoseStamped& msg_pose)
 {
-    geometry_msgs::PoseStamped msg_new = msg_pose;
-    geometry_msgs::Point point = msg_pose.pose.position;
-
-    if(!pos_received) {
-        pose_prev.position.x = msg_pose.pose.position.x;
-        pose_prev.position.y = msg_pose.pose.position.y;
-        pose_prev.position.z = msg_pose.pose.position.z;
-        pose_prev.orientation.z = msg_pose.pose.orientation.z;
-
-        pos_received = true;
-    }
+    geometry_msgs::Vector3 msg_violation;
 
     //Check for violation of capture volume limits
-    if(!(std::abs(point.x) < X_LIMIT)) point.x = pose_prev.position.x;
-    else if(!(std::abs(point.y) < Y_LIMIT)) point.y = pose_prev.position.x;
-    else if(!(std::abs(point.z) < Z_LIMIT)) point.z = pose_prev.position.x;
+    if(std::abs(msg_pose.pose.position.x) > X_LIMIT) msg_violation.x = 1;
+    else if(std::abs(msg_pose.pose.position.y) > Y_LIMIT) msg_violation.y = 1;
+    else if(std::abs(msg_pose.pose.position.z) > Z_LIMIT) msg_violation.z = 1;
+    pub_violation.publish(msg_violation);
 
-    //Check for value spikes
-    if(abs(abs(point.x)-abs(pose_prev.position.x)) > SPIKE_LIMIT) point.x = pose_prev.position.x;
-    if(abs(abs(point.x)-abs(pose_prev.position.x)) > SPIKE_LIMIT) point.y = pose_prev.position.y;
-    if(abs(abs(point.x)-abs(pose_prev.position.x)) > SPIKE_LIMIT) point.z = pose_prev.position.z;
-
-    //Update the position value in mocap data
-    msg_new.pose.position.x = point.x;
-    msg_new.pose.position.y = point.y;
-    msg_new.pose.position.z = point.z;
-
-    pose_prev = msg_new.pose;
-
-    pub_mocap.publish(msg_new);
+    pub_mocap.publish(msg_pose);
 }
