aa6c482596fcfcaf229ec6ddcbdb8bef34d1cfb8
Karanvir Panesar <singh.karanvir21@gmail.com>
2016-03-31 10:20:56 -0700

    Fixed compilation errors on master.

--- src/rqt_gcs/src/rqt_gcs/gps_demo.cpp
+++ src/rqt_gcs/src/rqt_gcs/gps_demo.cpp
@@ -8,6 +8,11 @@
 #include <mavros/mavros.h>
 #include <sensor_msgs/NavSatFix.h>
 
+geometry_msgs::Pose pos1, pos2;
+
+void StateCallback1(const geometry_msgs::PoseStamped& msg_pos) { pos1 = msg_pos.pose; }
+void StateCallback2(const geometry_msgs::PoseStamped& msg_pos) { pos2 = msg_pos.pose; }
+
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "gps_demo");
@@ -25,8 +30,8 @@ int main(int argc, char **argv)
   geometry_msgs::PoseStamped pos1;
   geometry_msgs::PoseStamped pos2;
 
-  ros::Subscriber subscriber1 = nh.subscribe("UAV1/mavros/mocap/pose", QUEUE_SIZE, StateCallback1());
-  ros::Subscriber subscriber2 = nh.subscribe("UAV2/mavros/mocap/pose", QUEUE_SIZE, StateCallback2());
+  ros::Subscriber subscriber1 = nh.subscribe("UAV1/mavros/mocap/pose", QUEUE_SIZE, StateCallback1);
+  ros::Subscriber subscriber2 = nh.subscribe("UAV2/mavros/mocap/pose", QUEUE_SIZE, StateCallback2);
 
   pub_pos_global1 = nh.advertise<sensor_msgs::NavSatFix>("UAV1/mavros/global_position/global",10);
   pub_pos_global2 = nh.advertise<sensor_msgs::NavSatFix>("UAV2/mavros/global_position/global",10);
@@ -51,9 +56,4 @@ int main(int argc, char **argv)
   }
 
 }
-
-
-
-      void StateCallback1(const geometry_msgs::PoseStamped& msg_pos) { pos1 = msg_pos.pose; }
-      void StateCallback2(const geometry_msgs::PoseStamped& msg_pos) { pos2 = msg_pos.pose; }
 
