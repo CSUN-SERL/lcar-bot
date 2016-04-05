2887a6caf6556e3f27f7a3b7b9a843e74f2bc147
mtbrobotanist <floydwin7@gmail.com>
2016-04-04 20:19:42 -0700

    uncommented object_detection node from jetson.launch and updated camera_subscriber and camera_publisher

--- src/machine_vision/src/ros_camera_publisher.cpp
+++ src/machine_vision/src/ros_camera_publisher.cpp
@@ -14,26 +14,28 @@ using namespace std;
 int main(int argc, char** argv)
 {
 
-  ros::init(argc, argv, "mono_cam", ros::init_options::AnonymousName);
+  ros::init(argc, argv, "cam", ros::init_options::AnonymousName);
   ros::NodeHandle nh;
   image_transport::ImageTransport it(nh);
 
   // get node specfic parameters from parameter server
 
   std::string topic;
-  bool paramSet = ros::param::get("~image_topic", topic);
+  bool paramSet = ros::param::get("~topic", topic);
 
   if(!paramSet){
-      topic = "mono_cam/image_raw";
+      ROS_ERROR_STREAM("a topic must be specified!\n\t ex: rosrun machine_vision camera_publisher _topic:=/cam1 or rosrun machine_vision camera_publisher _topic:=/cam1 _video_id:=1");
+      return 1;
   }
   image_transport::Publisher pub = it.advertise(topic, 1);
-
+  ROS_INFO_STREAM("publishing on topic: " << topic);
+  
   int video_source;
   paramSet = ros::param::get("~video_id", video_source);
   if(!paramSet){
       video_source = -1;
   }
-  cout << video_source << "\n";
+  ROS_INFO_STREAM("video source: " << video_source);
   cv::VideoCapture cap(video_source);
   // Check if video device can be opened with the given index
   if(!cap.isOpened()){
@@ -46,22 +48,19 @@ int main(int argc, char** argv)
   int numFrames = 0;
   while (nh.ok()) {
     try{
-        cout << "attempting frame capture\n";
         cap >> frame;
     // Check if grabbed frame is actually full with some content
         if(!frame.empty()) {
-            msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
+            msg = cv_bridge::CvImage(std_msgs::Header(), "rgb8", frame).toImageMsg();
             pub.publish(msg);
-
-            cout << numFrames++ << endl;
+            numFrames++;
         }
-        else cout << "frame skipped\n";
+        else ROS_ERROR_STREAM("frame skipped: " << numFrames);
     }
     catch(cv::Exception e){
         e.formatMessage();
         ROS_ERROR("%s", e.what());
     }
     ros::spinOnce();
-    //loop_rate.sleep();
   }
 }
--- src/machine_vision/src/ros_camera_subscriber.cpp
+++ src/machine_vision/src/ros_camera_subscriber.cpp
@@ -3,16 +3,18 @@
 #include "opencv2/highgui.hpp"
 #include <cv_bridge/cv_bridge.h>
 
+std::string topic_;
+
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
   try
   {
-    cv::imshow("view", cv_bridge::toCvShare(msg)->image);
+    cv::imshow(topic_, cv_bridge::toCvShare(msg,"rgb8")->image);
     cv::waitKey(1);
   }
   catch (cv_bridge::Exception& e)
   {
-    ROS_ERROR("Error in image subsrcriber: %s", e.what());
+    ROS_ERROR("Error in image subscriber: %s", e.what());
   }
 }
 
@@ -20,17 +22,23 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "image_listener", ros::init_options::AnonymousName);
   ros::NodeHandle nh;
-  cv::namedWindow("view");
-  cv::startWindowThread();
   image_transport::ImageTransport it(nh);
 
-  std::string topic;
-  if(argc >= 2)
-      topic = argv[1];
-  else topic = "mono_cam/image_raw";
+  
+  bool paramSet = ros::param::get("~topic", topic_);
 
-  image_transport::Subscriber sub = it.subscribe(topic, 1, imageCallback);
+  if(!paramSet){
+      ROS_ERROR_STREAM("a topic must be specified!\n\t ex: rosrun machine_vision camera_subscriber _topic:=/cam1");
+      return 1;
+  }
+
+  cv::namedWindow(topic_, CV_WINDOW_KEEPRATIO);
+  cv::startWindowThread();
+  
+  image_transport::Subscriber sub = it.subscribe(topic_, 5, imageCallback);
+  ROS_INFO_STREAM("subscribed to topic: " << topic_);
+  
   ros::spin();
-  cv::destroyWindow("view");
+  cv::destroyWindow(topic_);
 }
    
