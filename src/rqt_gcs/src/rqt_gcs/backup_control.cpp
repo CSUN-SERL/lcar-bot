d4eef839b319e4c06074ed915f764e2743a3fea5
Karanvir Panesar <singh.karanvir21@gmail.com>
2016-06-23 13:05:40 -0700

    Added functionality to keep UAV in place when the UAV stops receiving heartbeats from the GCS.

--- src/rqt_gcs/src/rqt_gcs/backup_control.cpp
+++ src/rqt_gcs/src/rqt_gcs/backup_control.cpp
@@ -4,7 +4,7 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "backup_control");
 
-  BackupManager manager(1);
+  BackupControl control(1);
   ros::Rate loop_rate(10); //10Hz
 
   while(ros::ok())
@@ -16,22 +16,48 @@ int main(int argc, char **argv)
   return 0;
 }
 
-BackupManager::BackupManager(int id)  //Class constructor
+BackupControl::BackupControl(int id)  //Class constructor
 {
-  //Setup heartbeat timer
-  timer_heartbeat = nh.createTimer(ros::Duration(0.1), &BackupManager::PublishHeartbeat, this);
+  //Setup heartbeat timers
+  timer_heartbeat_uav = nh.createTimer(ros::Duration(0.1), &BackupControl::PublishHeartbeat, this);
+  timer_heartbeat_gcs = nh.createTimer(ros::Duration(0.25), &BackupControl::AssumeControl, this);
   
   //this topic should be advertised under the /UAV* namsepace, eg. /UAV1/heartbeat/uav
   pub_heartbeat = nh.advertise<std_msgs::Int32>("heartbeat/uav", 0);
+  pub_sp_position = nh.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local", 10);
+
+  sub_heartbeat = nh.subscribe("heartbeat/gcs", 0, &BackupControl::ReceiveHeartbeat, this);
+  sub_pos_local  = nh.subscribe("mavros/local_position/pose", 0, &BackupControl::LocalPosCallback, this);
 }
 
-BackupManager::~BackupManager()
+BackupControl::~BackupControl()
 {
     //Default destructor
 }
 
-void BackupManager::PublishHeartbeat(const ros::TimerEvent& e)
+void BackupControl::PublishHeartbeat(const ros::TimerEvent &e)
 {
-  heartbeat.data++;
-  pub_heartbeat.publish(heartbeat);
+  heartbeat_uav.data++;
+  pub_heartbeat.publish(heartbeat_uav);
+}
+
+void BackupControl::ReceiveHeartbeat(const std_msgs::Int32 &msg_hb)
+{
+    //Restart Timer
+    timer_heartbeat_gcs.stop();
+    timer_heartbeat_gcs.start();
+
+    //Update previous position
+    pose_prev = pose_local;
+}
+
+void BackupControl::AssumeControl(const ros::TimerEvent &e){
+    //Create the message object
+    geometry_msgs::PoseStamped position_stamped;
+
+    //Update the message with the latest previous position
+    position_stamped.pose = pose_prev;
+
+    //Publish the message
+    pub_sp_position.publish(position_stamped);
 }
