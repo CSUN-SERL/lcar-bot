699e09c828c978b65bb7d868f8f7fcc024f14588
Nate <floydwin7@gmail.com>
2017-02-03 14:11:57 -0800

    edited uav_control_test_node

--- src/vehicle/nodes/uav_control_test_node.cpp
+++ src/vehicle/nodes/uav_control_test_node.cpp
@@ -5,7 +5,6 @@ int main(int argc, char **argv)
    ros::init(argc, argv, "uav_control_test");
    gcs::UAVControl quad1{2000};
 
-   ros::Rate loop_rate(10); //10Hz 
 
 //    lcar_msgs::TargetGlobal target_pt;
 //    target_pt.target.latitude = 47.3977255;
@@ -22,6 +21,7 @@ int main(int argc, char **argv)
    quad1.Arm(true);
    quad1.ScoutBuilding(target_pt);
 
+   ros::Rate loop_rate(10); //10Hz 
    while(ros::ok())
    {
        quad1.Run();
