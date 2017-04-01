33fd6fa1b5d4f17300a8c2e268ee7616dd427cd9
Nate <floydwin7@gmail.com>
2017-03-31 21:49:54 -0700

    conditional logic for osgearth

--- src/gcs/src/lib/gcs_main_window.cpp
+++ src/gcs/src/lib/gcs_main_window.cpp
@@ -628,14 +628,14 @@ void GCSMainWindow::OnAddVehicleTriggered()
 void GCSMainWindow::InitMap()
 {
 #ifdef USEOSGEARTH
-    //    osg::ref_ptr<osg::Node> node = osgDB::readFile<osg::Node>("aero-chart-arcgis.earth");
-//    osg_map = new osgEarth::QtGui::ViewerWidget(node.get());
-//    
-//    QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-//    sizePolicy.setHorizontalStretch(1);
-//    sizePolicy.setVerticalStretch(1);
-//    osg_map->setSizePolicy(sizePolicy);
-//    widget.layout_osg->addWidget(osg_map, 1);
+    osg::ref_ptr<osg::Node> node = osgDB::readFile<osg::Node>("aero-chart-arcgis.earth");
+    osg_map = new osgEarth::QtGui::ViewerWidget(node.get());
+    
+    QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    sizePolicy.setHorizontalStretch(1);
+    sizePolicy.setVerticalStretch(1);
+    osg_map->setSizePolicy(sizePolicy);
+    widget.layout_osg->addWidget(osg_map, 1);
 #else
     QString s = ros::package::getPath("gcs").c_str();
     QString map_url = QString("file://%1/map/uavmap.html").arg(s);
